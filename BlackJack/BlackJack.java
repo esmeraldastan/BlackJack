49b3e5f4f6fe902b263441eccc5034f41da7bcc3
Chase <esmeraldarekz99@gmail.com>
2016-09-15 13:29:05 -0700

    BJ

---
+++ BlackJack/BlackJack.java
@@ -0,0 +1,16 @@
+
+/**
+ * Write a description of class BlackJack here.
+ * 
+ * @author Esmeralda 
+ * @version 9.13.16
+ */
+import java.lang.Math;
+public class BlackJack{
+   public static void manin(String[] args){
+       
+    System.out.println("Welcome To The Game Of Black Jack!");
+        
+       
+    }
+}
